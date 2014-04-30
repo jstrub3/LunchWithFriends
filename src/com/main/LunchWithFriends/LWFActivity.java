481a71680953662ce5dd5ca378043ff0b4ffc533
Joseph Strubhart <jstrub3@uic.edu>
2014-04-29 21:15:47 -0500

    - Changing dinerActivity's current balance text to a button taking you to the balanceActivity.

---
+++ src/com/main/LunchWithFriends/LWFActivity.java
@@ -0,0 +1,15 @@
+package com.main.LunchWithFriends;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by Joseph on 4/29/14.
+ */
+public class LWFActivity extends Activity
+{
+}
